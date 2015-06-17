b8bf73286b9958ba7c9e9129a8f409b6dbaccb84
荒家智樹 <araie@s14.yeskist.net>
2015-06-17 15:26:47 +0900

    以前に作成したプロトタイプをDxライブラリの方へ移行しました。 コアラの画像は仮のものです。

--- ConversationProject/Source.cpp
+++ ConversationProject/Source.cpp
@@ -1,9 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
-#include <string.h>
+#include "DxLib.h"
 #include <windows.h>		//bool使うため
-#include <conio.h>			//_kbhit()を使うため
 #include <time.h>
 
 #define NUMBER_GEN 10			// keywordAndAnswer.txtのキーワード数:20
@@ -28,7 +26,17 @@ bool checkQ(char str[]){		// 疑問形かどうかチェックする関数
 }
 
 
-int main(void){
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR
+	lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(true);
+
+	if (DxLib_Init() == -1)        // ＤＸライブラリ初期化処理
+	{
+		return -1;            // エラーが起きたら直ちに終了
+	}
+
+	SetDrawScreen(DX_SCREEN_BACK);
 
 	FILE *fpGen, *fpGr, *fpEn;
 
@@ -40,13 +48,21 @@ int main(void){
 	int numberOfGreeting = 0;
 	int numberOfEncourage = 0;
 
-	char input[50];
+	char input[41] = "何か話しかけて!";
 
 	int judge = 0;
-	int greetingFlag = 0;
 	int count = 0;
 	int counter = 0;
 	int random = 0;
+
+	int greetingFlag = 0;
+	int flag = 0;
+	int EndFlag = 0;
+
+	// キー入力ハンドル用
+	int InputHandle;
+	int Red = GetColor(255, 0, 0);
+	int White = GetColor(255, 255, 255);
 
 	// ランダム値生成用
 	srand((unsigned int)time(NULL));
@@ -55,14 +71,17 @@ int main(void){
 	fpGr = fopen("greeting.txt", "r");
 	fpEn = fopen("encourage.txt", "r");
 
+
 	if ((fpGen == NULL) | (fpGr == NULL)){
-		printf("ファイルをオープンできませんでした。\n");
+		DrawFormatString(140, 200, Red, "ファイルをオープンできませんでした。\n");
 		return 1;
 	}
 
+
 	// ファイルのデータを構造体の配列に読み込む
 	for (int i = 0; i < NUMBER_GEN; i++){
-		judge = fscanf(fpGen, "%s %s %s %s", general[i].keyword, general[i].answerQ1, general[i].answerQ2, general[i].answerP);		// 1バイト分自動的に減らされて取り込まれる
+		judge = fscanf(fpGen, "%s %s %s %s", general[i].keyword,
+			general[i].answerQ1, general[i].answerQ2, general[i].answerP);        // 1バイト分自動的に減らされて取り込まれる
 		if (judge == -1) break;
 		numberOfKeyword++;
 	}
@@ -80,92 +99,129 @@ int main(void){
 		numberOfEncourage++;
 	}
 
+	int Handle;     // 画像入力ハンドル用
+	Handle = LoadGraph("Koala.jpg"); // 画像をロード
 
-	/*
-	// 用意されているデータの確認用に表示
-	for (int i = 0; i < numberOfKeyword; i++){
-	printf("%s %s %s %s\n", general[i].keyword, general[i].answerQ1, general[i].answerQ2, general[i].answerP);
-	}
+	// キー入力ハンドルを作る(キャンセルなし全角文字有り数値入力じゃなし)
+	InputHandle = MakeKeyInput(50, FALSE, FALSE, FALSE);
 
-	for (int i = 0; i < numberOfGreeting; i++){
-	printf("%s\n", greeting[i]);
-	}
+	while (ProcessMessage() == 0)
+	{
 
-	for (int i = 0; i < numberOfEncourage; i++){
-	printf("%s\n", encourage[i]);
-	}
-	*/
+		greetingFlag = 0; // greetingFlagが 1 で戻ってきたときに 0 で初期化
 
-	while (1){
+		ClearDrawScreen();
 
-		greetingFlag = 0;		// greetingFlagが 1 で戻ってきたときに 0 で初期化
-		count = 0;
-		counter = 0;
+		DrawGraph(170, 20, Handle, TRUE);
+		DrawString(170, 400, "あなた:", White);
 
-		printf("あなた: ");
+		if (flag == 0){
 
-		while (1){
-			if (_kbhit()){
-				scanf("%s",input);
-				break;
-			}
-			count++;
-			if (count % 600000 == 0){
-				random = rand() % numberOfEncourage;	// 0 ～ (numberOfEncourage-1)の間のランダム値生成
-				printf("\nKISTくん: %s\nあなた: ", encourage[random]);
-			}
+			SetActiveKeyInput(InputHandle);
+			flag = 1;
+
 		}
 
-		//挨拶
-		for (int i = 0; i < numberOfGreeting; i++){
-			if (strstr(input, greeting[i]) != NULL)
+		if (flag == 1){
+
+			CheckKeyInput(InputHandle);
+
+			DrawFormatString(170, 280, Red, "KISTくん: %s", input);
+
+			if (EndFlag == 1)
 			{
-				printf("KISTくん: %s\n", input);
-				greetingFlag = 1;
-				break;
-			}
-		}
-
-		if (greetingFlag == 0){
-
-			// 終了するとき↓
-			if (strstr(input, "bye") != NULL) {
-				printf("KISTくん: bye-bye! (^-^)/~~ \n");
+				ScreenFlip();
+				Sleep(2000);
 				break;
 			}
 
-			//疑問文に対応↓
-			if (checkQ(input)){
+			// 入力途中の文字列を描画
+			DrawKeyInputString(230, 400, InputHandle);
 
-				for (int i = 0; i < numberOfKeyword; i++){
-					if (strstr(input, general[i].keyword) != NULL)
-					{
-						printf("KISTくん: %s\n", general[i].answerQ1);
-						break;
-					}
-					counter++;
-				}
-				if (counter == numberOfKeyword)printf("KISTくん: ・・・。\n");	//キーワードがなかったら kist: ･･･
+			count++;
+
+			if (count % 200 == 0){
+				random = rand() % numberOfEncourage;	// 0 ～ (numberOfEncourage-1)の間のランダム値生成
+				strcpy(input, encourage[random]);
+				if ((random == 2) | (random == 4))
+					Handle = LoadGraph("angryKoala.jpg");
+				else Handle = LoadGraph("Koala.jpg");
 			}
-			//肯定文に対応↓
-			else {
-				for (int i = 0; i < numberOfKeyword; i++){
-					if (strstr(input, general[i].keyword) != NULL)
+
+			// 入力が終了している場合は終了
+			if (CheckKeyInput(InputHandle) != 0){
+
+				flag = 0;
+				count = 0;
+				Handle = LoadGraph("Koala.jpg");
+
+				// 入力された文字列を取得
+				strcpy(input, "");
+				GetKeyInputString(input, InputHandle);
+
+				// 用済みのインプットハンドルを削除する
+				DeleteKeyInput(InputHandle);
+
+				// キー入力ハンドルを作る(キャンセルなし全角文字有り数値入力じゃなし)
+				InputHandle = MakeKeyInput(50, FALSE, FALSE, FALSE);
+
+
+				for (int i = 0; i < numberOfGreeting; i++){
+					if (strstr(input, greeting[i]) != NULL)
 					{
-						printf("KISTくん: %s\n", general[i].answerP);
+						greetingFlag = 1;
 						break;
 					}
-					counter++;
 				}
-				if (counter == numberOfKeyword)printf("KISTくん: ・・・。\n");
+
+				if (greetingFlag == 0){
+
+					if (strstr(input, "bye") != NULL) {
+						char bye[41] = "バイバ～イ!";
+						strcpy(input, bye);
+						Handle = LoadGraph("byeKoala.jpg");
+						EndFlag = 1;
+					}
+					if (EndFlag != 1){
+						if (checkQ(input)){
+
+							for (int i = 0; i < numberOfKeyword; i++){
+								if (strstr(input, general[i].keyword) != NULL)
+								{
+									strcpy(input, general[i].answerQ1);
+									break;
+								}
+								counter++;
+							}
+
+							if (counter == numberOfKeyword)strcpy(input, "・・・。");
+						}
+
+						else {
+							for (int i = 0; i < numberOfKeyword; i++){
+								if (strstr(input, general[i].keyword) != NULL)
+								{
+									strcpy(input, general[i].answerP);
+									break;
+								}
+								counter++;
+							}
+
+							if (counter == numberOfKeyword)strcpy(input, "・・・。");
+						}
+					}
+				}
 			}
 		}
+
+		ScreenFlip();
 	}
 
 	fclose(fpGen);
 	fclose(fpGr);
 	fclose(fpEn);
 
-	return 0;
+	DxLib_End();
 
+	return 0;
 }
